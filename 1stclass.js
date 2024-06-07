673c989472d587fbf821d4fe34db4482940e4e81
aasiksifran17 <aasikmohamed782@gmail.com>
2024-06-07 15:29:21 +0530

    1stclass

--- 1stclass.js
+++ 1stclass.js
@@ -1,2 +1,12 @@
-console.log(10);
-
+for (let i = 1; i <= 100; i++) 
+    {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
